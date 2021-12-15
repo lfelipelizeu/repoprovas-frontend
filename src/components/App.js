be6ab8e7f78c2a71e03e00ff25a309816eae7ec9
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-15 14:44:35 -0300

    Add the home component as a route at the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home.js';
+
 function App() {
   return (
-    <>
-      Teste
-    </>);
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
