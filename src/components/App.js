97cf87c10851973a5f534c5380e012d4726b37e2
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-19 23:02:54 -0300

    Add the component to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home.js';
 import NewTest from './NewTest/NewTest.js';
+import ViewTestsOptions from './ViewTestsOptions.js';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<NewTest />} />
+        <Route path='/search' element={<ViewTestsOptions />} />
       </Routes>
     </BrowserRouter>);
 }
