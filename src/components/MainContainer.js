dac46903063f042913764402729994520f0886b9
Luis Felipe <l_felipelizeu@hotmail.com>
2022-01-24 22:45:52 -0300

    Create a main container component

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import palette from '../styles/Colors';
+
+const MainContainer = styled.main`
+    width: 100vw;
+    height: 100vh;
+    color: ${palette.ivory};
+    background-color: ${palette.blackCoffee};
+    overflow: hidden;
+`;
+
+export default MainContainer;
